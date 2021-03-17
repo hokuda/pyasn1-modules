bd798bff01211a761caae3224f6fae2d52652a9a
russhousley <housley@vigilsec.com>
2021-03-17 16:26:11 -0400

    Add support for RFC 7836

--- pyasn1_modules/rfc4357.py
+++ pyasn1_modules/rfc4357.py
@@ -2,8 +2,9 @@
 # This file is part of pyasn1-modules software.
 #
 # Created by Russ Housley with assistance from asn1ate v.0.6.0.
+# Modified by Russ Housley to allow gost_28147_param_Z from RFC 7836.
 #
-# Copyright (c) 2019, Vigil Security, LLC
+# Copyright (c) 2019-2021, Vigil Security, LLC
 # License: http://snmplabs.com/pyasn1/license.html
 #
 # Additional Cryptographic Algorithms for Use with GOST 28147-89,
@@ -11,6 +12,7 @@
 #
 # ASN.1 source from:
 # https://www.rfc-editor.org/rfc/rfc4357.txt
+# https://www.rfc-editor.org/rfc/rfc7863.txt
 # https://www.rfc-editor.org/errata/eid5927
 # https://www.rfc-editor.org/errata/eid5928
 #
@@ -22,6 +24,7 @@ from pyasn1.type import tag
 from pyasn1.type import univ
 
 from pyasn1_modules import rfc5280
+from pyasn1_modules import rfc7836
 
 
 # Import from RFC 5280
@@ -205,7 +208,8 @@ Gost28147_89_ParamSet.subtypeSpec = constraint.SingleValueConstraint(
     id_Gost28147_89_CryptoPro_D_ParamSet,
     id_Gost28147_89_CryptoPro_Oscar_1_1_ParamSet,
     id_Gost28147_89_CryptoPro_Oscar_1_0_ParamSet,
-    id_Gost28147_89_CryptoPro_RIC_1_ParamSet
+    id_Gost28147_89_CryptoPro_RIC_1_ParamSet,
+    rfc7836.id_tc26_gost_28147_param_Z
 )
 
 
